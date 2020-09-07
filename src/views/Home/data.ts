6a69c7f308f18b3ef099cf783bf0fec052324958
Renan Henrique <henriquebasshvf@gmail.com>
2020-09-07 20:03:10 -0300

    update message a new template

--- src/views/Home/data.ts
+++ src/views/Home/data.ts
@@ -2,9 +2,8 @@ export default {
   TEXT1: 'yarn g',
   TEXT2: ', para gerar componentes',
   TEXT3: "• View, to create Views(Container's), with redux.",
-  TEXT4:
-    '• Components, for the creation of components.',
-  TEXT5: '• Redux, to create a new Redux/Actions/Sagas.',
+  TEXT4: '• Components, for the creation of components.',
+  TEXT5: '• Hooks, to create a new Hooks.',
   TEXT6:
     '• Flow, to create a new application flow, already as a standard view, with redux.',
-}
+};
