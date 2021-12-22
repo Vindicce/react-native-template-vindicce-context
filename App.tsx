20fbfc2d8a4055f20fac892072a2744550e118dd
Renan Henrique <henriquebasshvf@gmail.com>
2021-12-22 13:58:13 -0300

    update

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { Store, ModalController } from './src/components';
+import { Store } from './src/components';
 import Routes from './src/routes/Routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
     <Store>
       <Routes />
-      <ModalController />
     </Store>
   </NavigationContainer>
 );
