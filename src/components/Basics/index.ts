85876ca85cf99a61fb0d2f3085648bc3807d6779
Renan O Henrique <henriquebasshvf@gmail.com>
2022-08-29 15:10:16 -0300

    feat: update

--- src/components/Basics/index.ts
+++ src/components/Basics/index.ts
@@ -1,5 +1,10 @@
 export { Page } from './Page';
+export { Page as P } from './Page';
 export { Wrapped } from './Wrapped';
+export { Wrapped as W } from './Wrapped';
 export { Text } from './Text';
+export { Text as T } from './Text';
 export { Linear } from './Linear';
+export { Linear as L } from './Linear';
 export { Icon } from './Icon';
+export { Icon as I } from './Icon';
